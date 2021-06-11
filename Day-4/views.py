605f8c27e6a8dab232dda68a7ac8a3ed6e18b64b
srikanya659qwe <51402676+srikanya659qwe@users.noreply.github.com>
2021-06-11 03:53:35 -0700

    Day-4
    Day-4

---
+++ Day-4/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hai(request):
+	return HttpResponse("hai guys welcome to Django session")
+
+def hello(request):
+	return HttpResponse("<h3 style=color:skyblue;background-color:red;font-size:20px>welcom to session</h3>")
+
+def sample(request):
+	n="apssdc"
+	return HttpResponse("welcome to" +n)
+
+def hm(request,name):
+	return HttpResponse('My name is:{}'.format(name))
+
+def basic(request,num):
+	return HttpResponse('My roll number is:{}'.format(num))
+
+def task(request,name,id):
+	return HttpResponse("My name is {} and my roll number is {}".format(name,id))
+
+def temp(request):
+	return render(request,'myApp/temp.html')
+
+def ls(request,name,id):
+	return render(request,'myApp/ls.html',{'n':name,'i':id})
