5a1c3c37597cbd188d652328ae32088c3021d31e
AmbikaPuvvula <45551971+AmbikaPuvvula@users.noreply.github.com>
2021-06-21 10:53:01 +0530

    Create, Read and Update Operations

---
+++ Day - 9 & 10/Vemu/CrudApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from CrudApp import views
+
+urlpatterns = [
+	path('demo/',views.demo,name = 'demo'),
+	path('register/',views.register,name='register'),
+	path('display/',views.display,name='display'),
+	path('update/<int:id>/',views.update,name='update'),
+]
